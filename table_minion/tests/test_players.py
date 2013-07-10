669410709b24884f712c4c6133778bffe9bb019c
Jeremy Thurgood <firxen@gmail.com>
2013-07-10 12:52:26 +0200

    Test player equality.

--- table_minion/tests/test_players.py
+++ table_minion/tests/test_players.py
@@ -7,13 +7,24 @@ from table_minion.players import Player, Players
 PLAYERS_CSV = '\r\n'.join([
     'name,team,1A,1B,2A,2B',
     'Gary Gygax,TSR,G,,G,',
-    'Joe Bloggs,TSR,P,,P,',
+    'Dave Arneson,TSR,P,,P,',
     'Jane Bloggs,,,X,,X',
     '',
 ])
 
 
 class TestPlayers(TestCase):
+    def test_player_equality(self):
+        player = Player('Gary Gygax', 'TSR', {'1A': 'G'})
+        self.assertEqual(player, player)
+        self.assertEqual(player, Player('Gary Gygax', 'TSR', {'1A': 'G'}))
+        self.assertNotEqual(player, 'Gary Gygax')
+        self.assertNotEqual(player, Player('Gary Glitter', 'TSR', {'1A': 'G'}))
+        self.assertNotEqual(player, Player('Gary Gygax', None, {'1A': 'G'}))
+        self.assertNotEqual(player, Player('Gary Gygax', 'WotC', {'1A': 'G'}))
+        self.assertNotEqual(player, Player('Gary Gygax', 'TSR', {}))
+        self.assertNotEqual(player, Player('Gary Gygax', 'TSR', {'1A': 'P'}))
+
     def test_from_csv(self):
         players = Players.from_csv(StringIO(PLAYERS_CSV))
 
@@ -21,7 +32,7 @@ class TestPlayers(TestCase):
         self.assertEqual('TSR', players.players[0].team)
         self.assertEqual({'1A': 'G', '2A': 'G'}, players.players[0].slots)
 
-        self.assertEqual('Joe Bloggs', players.players[1].name)
+        self.assertEqual('Dave Arneson', players.players[1].name)
         self.assertEqual('TSR', players.players[1].team)
         self.assertEqual({'1A': 'P', '2A': 'P'}, players.players[1].slots)
 
@@ -32,7 +43,7 @@ class TestPlayers(TestCase):
     def test_to_csv(self):
         players = Players([
             Player('Gary Gygax', 'TSR', {'1A': 'G', '2A': 'G'}),
-            Player('Joe Bloggs', 'TSR', {'1A': 'P', '2A': 'P'}),
+            Player('Dave Arneson', 'TSR', {'1A': 'P', '2A': 'P'}),
             Player('Jane Bloggs', None, {'1B': 'X', '2B': 'X'}),
         ])
 
