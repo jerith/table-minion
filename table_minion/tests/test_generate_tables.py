a14c4430463f4f434c32835661f5687ce7ac0468
Jeremy Thurgood <firxen@gmail.com>
2013-07-13 17:11:31 +0200

    Start of table generator tests.

--- table_minion/tests/test_generate_tables.py
+++ table_minion/tests/test_generate_tables.py
@@ -7,14 +7,29 @@ from table_minion.generate_tables import (
 
 class TestGameTablesGenerator(TestCase):
     # TODO: These tests.
-    def test_generate_tables(self):
+
+    def test_generate_two_full_tables(self):
         [game] = list(make_games(['1A']))
         players = make_players([
-                (10, 'Alpha', None, {'1A': 'P'}),
-                (2, 'Able', None, {'1A': 'X'}),
-                (2, 'Ares', 'Olympians', {'1A': 'P'}),
-                ])
-
-        print game
+            (2, 'GM', None, {'1A': 'G'}),
+            (12, 'Player', None, {'1A': 'P'}),
+        ])
         generator = GameTablesGenerator(game, players)
-        print generator.generate_tables()
+
+        [table1, table2] = generator.generate_tables()
+
+        self.assertNotEqual(table1, table2)
+        self.assertNotEqual(table1.gm, table2.gm)
+        self.assertNotEqual(table1.players, table2.players)
+
+        self.assertEqual(game, table1.game)
+        self.assertNotEqual(None, table1.gm)
+        self.assertEqual(6, len(table1.players))
+        self.assertEqual([], table1.warnings)
+        self.assertEqual([], table1.info)
+
+        self.assertEqual(game, table2.game)
+        self.assertNotEqual(None, table2.gm)
+        self.assertEqual(6, len(table2.players))
+        self.assertEqual([], table2.warnings)
+        self.assertEqual([], table2.info)
